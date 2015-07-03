5bf39d579b217580831fc92ef2468dd0f656022c
Cam Parry <cam.parry@gmail.com>
2015-07-03 14:31:31 +0100

    Add test script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Apollo-ELK
-ELK stack running in standalone mode with docker, weave and consul
+Apollo-ELK
+=========
+
+ELK (Elasticsearch, Logstash & Kibana) stack running in standalone mode with docker compose
+
+#### Download Apollo ELK
+
+##### Install from source at head
+1. ```git clone https://github.com/Capgemini/Apollo-ELK.git```
+2. ```cd Apollo-ELK```
+3. ```pip install -r requirements.txt```
---
+++ sendtestmessage.py
@@ -0,0 +1,26 @@
+import socket
+import json
+import sys
+ 
+HOST = '10.10.10.100'
+PORT = 9563
+ 
+try:
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+  sys.stderr.write("[ERROR] %s\n" % msg[1])
+  sys.exit(1)
+ 
+try:
+  sock.connect((HOST, PORT))
+except socket.error, msg:
+  sys.stderr.write("[ERROR] %s\n" % msg[1])
+  sys.exit(2)
+ 
+msg = {'@message': 'python test message', '@tags': ['python', 'test']}
+ 
+sock.send(json.dumps(msg))
+ 
+sock.close()
+sys.exit(0)
+
